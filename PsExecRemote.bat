40b1ad4a294a2a40ab6571ca1987e97d5a331c31
vijayphilip@gmail.com <vijayphilip@gmail.com>
2012-10-04 11:20:17 -0400

    Initial Commit
    Version 0.1

---
+++ PsExecRemote.bat
@@ -0,0 +1,36 @@
+@echo off
+
+SET IPA=23.23.23.23
+SET USER=Administrator
+SET PSWD="************"
+SET EXE="ipconfig"
+
+
+SET VERSION=0.1
+SET AUTHOR=Vijay J. Philip
+SET CREDITS=PsExec v1.98 By Mark Russinovich
+SET STARTTIME=%date% : %time%
+@echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+@echo Remote EC2: %IPA%
+@echo Remote User: %USER% : Password:%PSWD%
+@echo Remote S3 Sync Script: %EXE%
+@echo Version: %VERSION%
+@echo Author: %AUTHOR%
+@echo Credits: %CREDITS%
+@echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+@echo Started at %STARTTIME%
+@echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ >> results.log
+@echo Started at %STARTTIME%	>> results.log
+@echo.
+@echo Results:
+@echo ------------------------------------------------------------------------------
+PsExec.exe \\%IPA% -u %USER%  -p %PSWD% %EXE% >> results.log
+@echo.
+@echo ------------------------------------------------------------------------------
+@echo.
+SET STOPTIME=%date% : %time%
+@echo Completed at %STOPTIME%  >> results.log
+@echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  >> results.log
+@echo Completed at %STOPTIME%
+@echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+pause
